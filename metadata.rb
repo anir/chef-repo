7d6898f220ff0b542172f3080643582de9a7bb2a
anir <anirban25987@gmail.com>
2018-01-18 04:09:39 +0000

    adding basic apache recipe

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'apache'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'anirban'
+maintainer_email 'anirban25987@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# installing the apache server
+package 'apache2' do
+	package_name 'httpd'
+	action :install
+end
+
+# starting the apache service
+service 'apache2' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
