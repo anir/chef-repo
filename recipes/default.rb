0e5ec5a48b1c2a0097e336999b5a20693415efa9
anir <anirban25987@gmail.com>
2018-01-21 06:10:27 +0000

    anir: added motd recipe and added package variables

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,13 +5,22 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # installing the apache server
+
+if node['platform_family'] == 'rhel'
+	package = 'httpd'
+elsif node['platform_family'] == 'debian'
+	package = 'apache2'
+end 
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
 # starting the apache service
 service 'apache2' do
-	service_name 'httpd'
+	service_name package
 	action [:start, :enable]
 end
+
+#include_recipe 'apache::websites'
